17b2aa6986f7052aec0cda700dae73ecd77d5b94
Eugene Moiseev <eugen.moiseev@gmail.com>
2020-06-18 13:32:24 +0300

    Ready test bot

--- handlers.py
+++ handlers.py
@@ -7,6 +7,7 @@ from aiogram.types import Message
 from aiogram.types.message import ContentType
 from config import admin_id
 from run import Run
+from utils import image_concat
 
 async def send_to_admin(dp):
     await bot.send_message(chat_id=admin_id, text='Bot started')
@@ -32,6 +33,7 @@ async def enter_run(message: Message):
 async def get_photo_1(message: Message, state: FSMContext):
     answer = '1'
     await state.update_data(answer1=answer)
+    await message.photo[-1].download('img/content.jpg')
     await message.answer('А теперь я жду от тебя второе фото, которое ты хочешь использовать в качестве стиля, либо выбери из предложенных')
 
     await Run.next()
@@ -41,10 +43,15 @@ async def get_photo_2(message: Message, state: FSMContext):
     answer2 = '2'
     data = await state.get_data()
     answer1 = data.get('answer1')
-
+    await message.photo[-1].download('img/style.jpg')
     await message.answer('Спасибо! Ожидайте результат...')
 
-    await message.answer(f'{answer1} and {answer2}')
+    #await message.answer(f'{answer1} and {answer2}')
 
+    image_concat('img/content.jpg', 'img/style.jpg')
+
+    photo = {'photo': open('img/result.jpg', 'rb')}
+    await message.answer_photo(photo=photo['photo'])
+    
     #await state.finish()
     await state.reset_state()
---
+++ utils.py
@@ -0,0 +1,17 @@
+import sys
+from PIL import Image
+import numpy as np
+
+def image_concat(img_1, img_2):
+    list_im = [img_1, img_2]
+    imgs = [ Image.open(i) for i in list_im ]
+    min_shape = sorted( [(np.sum(i.size), i.size ) for i in imgs])[0][1]
+    imgs_comb = np.hstack( (np.asarray( i.resize(min_shape) ) for i in imgs ) )
+
+    imgs_comb = Image.fromarray( imgs_comb)
+    imgs_comb.save( 'img/result.jpg' )    
+
+
+
+
+    
