596e1040a9cd994dc15ead1805e3aae120cfd08c
Eugene Moiseev <eugen.moiseev@gmail.com>
2020-06-24 00:01:06 +0300

    add config

---
+++ config.py
@@ -0,0 +1,6 @@
+import aiohttp
+
+#PROXY_URL = 'socks5://178.128.203.1:1080'
+#PROXY_AUTH= aiohttp.BasicAuth(login='student', password='TH8FwlMMwWvbJF8FYcq0')
+BOT_TOKEN = '1166530243:AAHGU7b0LrXe-u4SXV--OCwYETtAW5sQp4A'
+admin_id = 171168424
