16ca55e7ac4b3dbb1b07d5de4b3aa9fb53950619
Eugene Moiseev <eugen.moiseev@gmail.com>
2020-06-24 00:16:21 +0300

    fix

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
-import aiohttp
+#import aiohttp
+import os
 
 #PROXY_URL = 'socks5://178.128.203.1:1080'
 #PROXY_AUTH= aiohttp.BasicAuth(login='student', password='TH8FwlMMwWvbJF8FYcq0')
-BOT_TOKEN = '1166530243:AAHGU7b0LrXe-u4SXV--OCwYETtAW5sQp4A'
+#BOT_TOKEN = '1166530243:AAHGU7b0LrXe-u4SXV--OCwYETtAW5sQp4A'
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 admin_id = 171168424
